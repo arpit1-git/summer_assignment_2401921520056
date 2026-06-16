7d8d576c9630de012b9f1e7143be3175a795b43e
arpit1-git <arpitraj1910@gmail.com>
2026-06-16 15:29:36 +0530

    week 1 day 3

---
+++ Week 1/day 3/Container_with_Most_Water.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int maxArea(int[] height) {
+        int i = 0;
+        int j = height.length - 1;
+        int maxwater = 0;
+        while (i < j) {
+            int w = j - i;
+            int h = Math.min(height[i], height[j]);
+            int area = w * h;
+            maxwater = Math.max(maxwater, area);
+            if (height[i] > height[j]) {
+                j--; 
+            }else {
+                i++;
+            }
+        }
+        return maxwater;
+    }
+}
