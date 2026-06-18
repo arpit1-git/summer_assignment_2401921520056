59a81f5e597c9be274207075a8b4aedd6e9d47ae
arpit1-git <arpitraj1910@gmail.com>
2026-06-18 12:38:43 +0530

    Square of sorted array

--- Week_1/day_3/Squares_of_Sorted_array.java
+++ Week_1/day_3/Squares_of_Sorted_array.java
@@ -2,7 +2,7 @@ package Week_1.day_3;
 
 import java.util.Arrays;
 
-class Solution {
+public class Squares_of_Sorted_array {
 
     public int[] sortedSquares(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
