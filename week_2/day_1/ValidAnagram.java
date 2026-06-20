f0e43bcb8b5202c0def330b68868d7ace7be8f2b
arpit1-git <arpitraj1910@gmail.com>
2026-06-20 20:25:46 +0530

    Valid Anagram

---
+++ week_2/day_1/ValidAnagram.java
@@ -0,0 +1,21 @@
+package week_2.day_1;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a']++;
+        }
+
+        for (char ch : t.toCharArray()) {
+            count[ch - 'a']--;
+        }
+
+        boolean allZeros = Arrays.stream(count).allMatch(element -> element == 0);
+        return allZeros;
+    }
+}
