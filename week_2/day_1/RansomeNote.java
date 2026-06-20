776e12a234e0b7c931c86c737237a0fffca088d8
arpit1-git <arpitraj1910@gmail.com>
2026-06-20 20:38:36 +0530

    Ransome Note

--- week_2/day_1/RansomeNote.java
+++ week_2/day_1/RansomeNote.java
@@ -1,5 +1,20 @@
 package week_2.day_1;
 
 public class RansomeNote {
-    
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[256];
+        for (int i = 0; i < magazine.length(); i++) {
+            freq[magazine.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < ransomNote.length(); i++) {
+            freq[ransomNote.charAt(i) - 'a']--;
+            if (freq[ransomNote.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+    
+
