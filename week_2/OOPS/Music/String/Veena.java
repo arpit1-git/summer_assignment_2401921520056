c8fe7a2f98408ba5886c1ce4c7b8c752aa37d5e7
arpit1-git <arpitraj1910@gmail.com>
2026-06-28 01:17:03 +0530

    Implement test class for Playable Interface

---
+++ week_2/OOPS/Music/String/Veena.java
@@ -0,0 +1,11 @@
+package week_2.OOPS.Music.String;
+
+import week_2.OOPS.Music.Playable;
+
+public class Veena implements Playable {
+
+    @Override
+    public void play() {
+        System.out.println("Playing Veena");
+    }
+}
